6f5e914ef67d82683338a80556279b75053290e8
c0argpat <c0argpat@taniroda-hp-1>
2025-09-24 09:41:42 +0200

    2

--- util/database.js
+++ util/database.js
@@ -2,11 +2,11 @@ import Database from "better-sqlite3"
 
 const  db = new Database("./data/database.sqlite")
 
-db.prepare(`CREATE TABLE IF NOT EXISTS teams (id PRIMARY KEY AUTOINCREMENT, name STRING, score INTEGER)`).run()
+db.prepare(`CREATE TABLE IF NOT EXISTS teams (id INTIGER PRIMARY KEY AUTOINCREMENT, name STRING, score INTEGER)`).run()
 
 export const getTeams = () => db.prepare('SELECT * FROM teams').all()
 export const getTeamById = (id) => db.prepare("SELECT * FROM teams WHERE id = ?").get(id)
-export const saveTeam = (name, score) => db.prepare("INSERT INTO teams (naem, score) VALUES (?, ?)").run(name,score)
+export const saveTeam = (name, score) => db.prepare("INSERT INTO teams (name, score) VALUES (?, ?)").run(name,score)
 export const updateTeam = (id, name, score) => db.prepare("UPDATE teams SET name=?, score=?, WHERE id = ?").run(name,score, id)
 export const deleteTeam = (id) => db.prepare("DELETE FROM teams WHERE id=?").run(id)
 
